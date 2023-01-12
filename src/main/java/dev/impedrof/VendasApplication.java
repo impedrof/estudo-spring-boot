04a8675222a79a4dae4ea21839138adf23a627f0
Pedro Freitas <pedrofreitas790@gmail.com>
2023-01-11 21:50:01 -0300

    Repositório de clientes refatorado para o JPA.

--- src/main/java/dev/impedrof/VendasApplication.java
+++ src/main/java/dev/impedrof/VendasApplication.java
@@ -23,10 +23,18 @@ public class VendasApplication {
             repo.salvar(new Cliente("Vayne"));
             repo.salvar(new Cliente("Draven"));
             System.out.println(repo.obterClientes());
-            repo.atualizar(new Cliente(2, "Draven ADC"));
+            Cliente draven = new Cliente(2, "Dravensssssssssss");
+            repo.atualizar(draven);
             System.out.println(repo.obterClientes());
-            Cliente cliente = repo.obterPorId(1).orElseThrow(() -> new RuntimeException("cliente não encontrado"));
-            System.out.println(cliente.getNome());
+
+            repo.deletar(2);
+            System.out.println(repo.obterClientes());
+
+            Cliente cliente = repo.obterPorId(1);
+            System.out.println(cliente);
+
+            System.out.println(repo.buscarPorNome("Draven"));
+            System.out.println(repo.obterClientes());
         };
     }
 
@@ -37,7 +45,7 @@ public class VendasApplication {
 
     @GetMapping("{id}")
     public Cliente obterPorId(@PathVariable int id) {
-        return this.repo.obterPorId(id).orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+        return this.repo.obterPorId(id);
     }
 
     @PostMapping()
--- src/main/java/dev/impedrof/domain/repositorio/Clientes.java
+++ src/main/java/dev/impedrof/domain/repositorio/Clientes.java
@@ -5,14 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Repository
 public class Clientes {
 
-    private static final String INSERT = "INSERT INTO CLIENTE (nome) VALUES (?)";
     private static final String SELECT_ALL = "SELECT * FROM CLIENTE";
     private static final String UPDATE = "UPDATE CLIENTE SET nome = ? WHERE id = ?";
     private static final String DELETE = "DELETE FROM CLIENTE WHERE id = ?";
@@ -20,28 +23,49 @@ public class Clientes {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional
     public void salvar(Cliente cliente) {
-        jdbcTemplate.update(INSERT, cliente.getNome());
+        entityManager.persist(cliente);
     }
 
+    @Transactional
     public void atualizar(Cliente cliente) {
-        jdbcTemplate.update(UPDATE, cliente.getNome(), cliente.getId());
+        entityManager.merge(cliente);
     }
 
+    @Transactional
+    public void deletar(Cliente cliente) {
+        if (!entityManager.contains(cliente)) {
+            entityManager.merge(cliente);
+        }
+        entityManager.remove(cliente);
+    }
+
+    @Transactional
     public void deletar(int id) {
-        jdbcTemplate.update(DELETE, id);
+        Cliente cliente = obterPorId(id);
+        deletar(cliente);
     }
 
-    public Optional<Cliente> obterPorId(int id) {
-        return jdbcTemplate.query(SELECT_ALL.concat(" WHERE id = ?"), getClienteRowMapper(), id).stream().findFirst();
+    @Transactional(readOnly = true)
+    public Cliente obterPorId(int id) {
+        return entityManager.find(Cliente.class, id);
     }
 
+    @Transactional(readOnly = true)
     public List<Cliente> buscarPorNome(String nome) {
-        return jdbcTemplate.query(SELECT_ALL.concat(" WHERE nome like ?"), getClienteRowMapper(), "%" + nome + "%");
+        String jpql = "SELECT c FROM Cliente c WHERE c.nome like :nome";
+        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
+        query.setParameter("nome", "%" + nome + "%");
+        return query.getResultList();
     }
 
+    @Transactional
     public List<Cliente> obterClientes() {
-        return jdbcTemplate.query(SELECT_ALL, getClienteRowMapper());
+        return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
     }
 
     private RowMapper<Cliente> getClienteRowMapper() {
