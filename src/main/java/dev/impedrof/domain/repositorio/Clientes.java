0e37910a5322beb44052649072a36e051c5d3a67
Pedro Freitas <pedrofreitas790@gmail.com>
2023-01-11 21:55:56 -0300

    Removidas as referências ao JDBC no repositório de clientes.

--- src/main/java/dev/impedrof/domain/repositorio/Clientes.java
+++ src/main/java/dev/impedrof/domain/repositorio/Clientes.java
@@ -2,26 +2,15 @@ package dev.impedrof.domain.repositorio;
 
 import dev.impedrof.domain.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Repository
 public class Clientes {
-
-    private static final String SELECT_ALL = "SELECT * FROM CLIENTE";
-    private static final String UPDATE = "UPDATE CLIENTE SET nome = ? WHERE id = ?";
-    private static final String DELETE = "DELETE FROM CLIENTE WHERE id = ?";
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     private EntityManager entityManager;
@@ -66,9 +55,5 @@ public class Clientes {
     @Transactional(readOnly = true)
     public List<Cliente> obterClientes() {
         return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
-    }
-
-    private RowMapper<Cliente> getClienteRowMapper() {
-        return (rs, rowNum) -> new Cliente(rs.getInt("id"), rs.getString("nome"));
     }
 }
