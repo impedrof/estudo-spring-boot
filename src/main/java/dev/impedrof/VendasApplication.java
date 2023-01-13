045f3abc718af4d9cf713e30a03ce93b350d07e9
Pedro Freitas <pedrofreitas790@gmail.com>
2023-01-13 19:28:03 -0300

    Refatoração do repositório de Clientes para JPA Repository.

--- src/main/java/dev/impedrof/VendasApplication.java
+++ src/main/java/dev/impedrof/VendasApplication.java
@@ -20,42 +20,42 @@ public class VendasApplication {
     @Bean
     public CommandLineRunner init(@Autowired Clientes repo) {
         return args -> {
-            repo.salvar(new Cliente("Vayne"));
-            repo.salvar(new Cliente("Draven"));
-            System.out.println(repo.obterClientes());
+            repo.save(new Cliente("Vayne"));
+            repo.save(new Cliente("Draven"));
+            System.out.println(repo.findAll());
             Cliente draven = new Cliente(2, "Dravensssssssssss");
-            repo.atualizar(draven);
-            System.out.println(repo.obterClientes());
+            repo.save(draven);
+            System.out.println(repo.findAll());
 
-            repo.deletar(2);
-            System.out.println(repo.obterClientes());
+            repo.deleteById(2);
+            System.out.println(repo.findAll());
 
-            Cliente cliente = repo.obterPorId(1);
+            Cliente cliente = repo.findById(1).orElseThrow();
             System.out.println(cliente);
 
-            System.out.println(repo.buscarPorNome("Draven"));
-            System.out.println(repo.obterClientes());
+            System.out.println(repo.findByNomeLike("Draven"));
+            System.out.println(repo.findAll());
         };
     }
 
     @GetMapping("/todos")
     public List<Cliente> obterTodos() {
-        return this.repo.obterClientes();
+        return this.repo.findAll();
     }
 
     @GetMapping("{id}")
     public Cliente obterPorId(@PathVariable int id) {
-        return this.repo.obterPorId(id);
+        return this.repo.findById(id).orElseThrow();
     }
 
     @PostMapping()
     public void atualizar(@RequestBody Cliente cliente) {
-        this.repo.atualizar(cliente);
+        this.repo.save(cliente);
     }
 
     @DeleteMapping("{id}")
     public void deletar(@PathVariable int id) {
-        this.repo.deletar(id);
+        this.repo.deleteById(id);
     }
 
     public static void main(String[] args) {
--- src/main/java/dev/impedrof/domain/repositorio/Clientes.java
+++ src/main/java/dev/impedrof/domain/repositorio/Clientes.java
@@ -1,59 +1,10 @@
 package dev.impedrof.domain.repositorio;
 
 import dev.impedrof.domain.entity.Cliente;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 import java.util.List;
 
-@Repository
-public class Clientes {
-
-    @Autowired
-    private EntityManager entityManager;
-
-    @Transactional
-    public void salvar(Cliente cliente) {
-        entityManager.persist(cliente);
-    }
-
-    @Transactional
-    public void atualizar(Cliente cliente) {
-        entityManager.merge(cliente);
-    }
-
-    @Transactional
-    public void deletar(Cliente cliente) {
-        if (!entityManager.contains(cliente)) {
-            entityManager.merge(cliente);
-        }
-        entityManager.remove(cliente);
-    }
-
-    @Transactional
-    public void deletar(int id) {
-        Cliente cliente = obterPorId(id);
-        deletar(cliente);
-    }
-
-    @Transactional(readOnly = true)
-    public Cliente obterPorId(int id) {
-        return entityManager.find(Cliente.class, id);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> buscarPorNome(String nome) {
-        String jpql = "SELECT c FROM Cliente c WHERE c.nome like :nome";
-        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
-        query.setParameter("nome", "%" + nome + "%");
-        return query.getResultList();
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> obterClientes() {
-        return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
-    }
+public interface Clientes extends JpaRepository<Cliente, Integer> {
+    List<Cliente> findByNomeLike(String draven);
 }
