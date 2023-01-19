d9aea635cdb3f5b8a366a990a00e173e84aae49b
Pedro Freitas <pedrofreitas790@gmail.com>
2023-01-19 20:45:58 -0300

    Mapeando a entidade Produto.

--- src/main/java/dev/impedrof/domain/entity/Produto.java
+++ src/main/java/dev/impedrof/domain/entity/Produto.java
@@ -1,11 +1,21 @@
 package dev.impedrof.domain.entity;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "produto")
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column()
     private Integer id;
+
+    @Column()
     private String descricao;
+
+    @Column(name = "preco_unitario")
     private BigDecimal precoUnitario;
 
     public Integer getId() {
