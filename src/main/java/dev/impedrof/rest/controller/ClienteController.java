26175756526c45e17972ca36c0657e6e67e96576
Pedro Freitas <pedrofreitas790@gmail.com>
2023-02-14 19:44:27 -0300

    Adicionadas as chamadas de salvar e buscar cliente no controller.

--- src/main/java/dev/impedrof/rest/controller/ClienteController.java
+++ src/main/java/dev/impedrof/rest/controller/ClienteController.java
@@ -1,18 +1,53 @@
 package dev.impedrof.rest.controller;
 
+import dev.impedrof.domain.entity.Cliente;
+import dev.impedrof.domain.repository.ClientesRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
-@RequestMapping("cliente")
+@RequestMapping("clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "{nome}", method = RequestMethod.GET)
+    private final ClientesRepository repo;
+
+    public ClienteController(ClientesRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping()
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente) {
-        return String.format("Hello %s", nomeCliente);
+    public ResponseEntity<?> findAll() {
+        try {
+            List<Cliente> clientes = repo.findAll();
+            return ResponseEntity.ok(clientes);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("{id}")
+    @ResponseBody
+    public ResponseEntity<?> getById(@PathVariable Integer id) {
+        Optional<Cliente> cliente = repo.findById(id);
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseBody
+    public ResponseEntity<?> save(@RequestBody Cliente cliente) {
+        try {
+            Cliente clienteSalvo = repo.save(cliente);
+            return ResponseEntity.ok(clienteSalvo);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
